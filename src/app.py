bb0725fc2380bad4a0207acc8f20ccd664ade4c5
hbarrada <barradahamza123@gmail.com>
2025-10-07 11:27:10 +0200

    fixed README

--- src/app.py
+++ src/app.py
@@ -1058,4 +1058,4 @@ if __name__ == "__main__":
         </html>
         """)
         
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
